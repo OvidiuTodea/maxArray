347772c006354bdcfd7befd9a0b3401d228eac53
ovidiu todea <ovi.todea@yahoo.com>
2019-12-07 12:56:53 +0200

    add exchange positions values into an array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,14 @@ public class Main {
         return givenArray[n-1];
     }
 
+      static int[] exchangePositions(int arr[], int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+        return arr;
+    }
+    
+
     public static void main(String[] args) {
 
         int arr[] = { 6, 3 , 7 , 8 , 10 , 4 , 5 };
@@ -17,7 +25,10 @@ public class Main {
         System.out.print("Maximul din array-ul " + str+ " este : " );
         System.out.println(maxArray(arr,n));
 
+        System.out.println("---------------");
+
+        exchangePositions(arr, 2, 3);
+        String str2 = Arrays.toString(arr);
+        System.out.println(str2);
     }
-
-
 }
